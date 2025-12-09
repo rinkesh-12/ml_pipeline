05320c8dc0ee09f4faae44d667011d4f2ad75392
rinkesh-12 <rankrinkesh@gmail.com>
2025-12-09 11:24:54 +0530

    Data Transformation

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,34 @@
 import os
 import sys
+
+import numpy as np
+import pandas as pd
+import pickle
+
+from src.exception import CustomException
+from src.logger import logging
+
+def save_object(file_path, obj):
+    """
+    Save a Python object to a file using pickle.
+
+    Args:
+        file_path (str): The path where the object should be saved.
+        obj: The Python object to be saved.
+
+    Raises:
+        CustomException: If there is an error during the saving process.
+    """
+    try:
+
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+
+        logging.info(f"Object saved successfully at {file_path}")
+
+    except Exception as e:
+        logging.error(f"Error occurred while saving object: {e}")
+        raise CustomException(e, sys)
